16a31cb8fb2bd036c0aac55fb223af88e3c22b40
Lucas Juarez <juarezlucas.92@gmail.com>
2022-09-13 21:04:12 -0300

    Primera entrega

--- tp_inicial/ej4_sqlite_treeview.py
+++ tp_inicial/ej4_sqlite_treeview.py
@@ -3,6 +3,7 @@ from tkinter.messagebox import *
 import sqlite3
 from tkinter import ttk
 import re
+
 # ##############################################
 # MODELO
 # ##############################################
@@ -15,25 +16,26 @@ import re
     def validar_cantidad(self,): pass"""
 
 
-
-
-
-
 def conexion():
     con = sqlite3.connect("mibase.db")
     return con
 
+
 def crear_tabla():
     con = conexion()
     cursor = con.cursor()
-    sql = """CREATE TABLE productos
+    sql = """CREATE TABLE gastos
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             producto varchar(20) NOT NULL,
-             cantidad real,
-             precio real)
+             fecha varchar2(16) NOT NULL,
+             tarjeta_credito varchar2(16),
+             descripcion varchar2 NOT NULL,
+             cuota_actual integer,
+             cuota_final integer,
+             monto real not null)
     """
     cursor.execute(sql)
     con.commit()
+
 
 try:
     conexion()
@@ -42,44 +44,44 @@ except:
     print("Hay un error")
 
 
-def alta(producto, cantidad, precio, tree):
-    cadena = producto
-    patron="^[A-Za-záéíóú]*$"  #regex para el campo cadena
-    if(re.match(patron, cadena)):
-        print(producto, cantidad, precio)
-        con=conexion()
-        cursor=con.cursor()
-        data=(producto, cantidad, precio)
-        sql="INSERT INTO productos(producto, cantidad, precio) VALUES(?, ?, ?)"
+def alta(fecha, tarjeta_credito, desc, cuota_actual, cuota_final, monto, tree):
+    cadena = tarjeta_credito
+    patron = "^[A-Za-záéíóú]*$"  # regex para el campo cadena
+    if re.match(patron, cadena):
+        print(fecha, tarjeta_credito, desc, cuota_actual, cuota_final, monto)
+        con = conexion()
+        cursor = con.cursor()
+        data = (fecha, tarjeta_credito, desc, cuota_actual, cuota_final, monto)
+        sql = "INSERT INTO gastos(fecha, tarjeta_credito, descripcion, cuota_actual, cuota_final, monto) VALUES(?, ?, ?, ?, ?, ?)"
         cursor.execute(sql, data)
         con.commit()
-        print("Estoy en alta todo ok")
         actualizar_treeview(tree)
     else:
         print("error en campo producto")
 
 
-def consultar():
-    global compra
-    print(compra)
+def consultar(tree):
+    actualizar_treeview(tree)
+
 
 def borrar(tree):
     valor = tree.selection()
-    print(valor)   #('I005',)
+    print(valor)  # ('I005',)
     item = tree.item(valor)
-    print(item)    #{'text': 5, 'image': '', 'values': ['daSDasd', '13.0', '2.0'], 'open': 0, 'tags': ''}
-    print(item['text'])
-    mi_id = item['text']
+    print(
+        item
+    )  # {'text': 5, 'image': '', 'values': ['daSDasd', '13.0', '2.0'], 'open': 0, 'tags': ''}
+    print(item["text"])
+    mi_id = item["text"]
 
-    con=conexion()
-    cursor=con.cursor()
-    #mi_id = int(mi_id)
+    con = conexion()
+    cursor = con.cursor()
+    # mi_id = int(mi_id)
     data = (mi_id,)
-    sql = "DELETE FROM productos WHERE id = ?;"
+    sql = "DELETE FROM gastos WHERE id = ?;"
     cursor.execute(sql, data)
     con.commit()
     tree.delete(valor)
-
 
 
 def actualizar_treeview(mitreview):
@@ -87,18 +89,27 @@ def actualizar_treeview(mitreview):
     for element in records:
         mitreview.delete(element)
 
-    sql = "SELECT * FROM productos ORDER BY id ASC"
-    con=conexion()
-    cursor=con.cursor()
-    datos=cursor.execute(sql)
+    sql = "SELECT id, fecha, tarjeta_credito, descripcion, cuota_actual, cuota_final, monto FROM gastos ORDER BY id ASC"
+    con = conexion()
+    cursor = con.cursor()
+    datos = cursor.execute(sql)
 
     resultado = datos.fetchall()
-    for fila in resultado:
-        print(fila)
-        mitreview.insert("", 0, text=fila[0], values=(fila[1], fila[2], fila[3]))
-
-
-
+    for registro in resultado:
+        print(registro)
+        mitreview.insert(
+            "",
+            0,
+            text=registro[0],
+            values=(
+                registro[1],
+                registro[2],
+                registro[3],
+                registro[4],
+                registro[5],
+                registro[6],
+            ),
+        )
 
 
 # ##############################################
@@ -106,53 +117,89 @@ def actualizar_treeview(mitreview):
 # ##############################################
 
 root = Tk()
-root.title("Tarea POO")
-        
-titulo = Label(root, text="Ingrese sus datos", bg="DarkOrchid3", fg="thistle1", height=1, width=60)
-titulo.grid(row=0, column=0, columnspan=4, padx=1, pady=1, sticky=W+E)
+root.title("Presupuesto personal")
 
-producto = Label(root, text="Producto")
+titulo = Label(root, text="v1.0", bg="DarkOrchid3", fg="thistle1", height=1, width=60)
+titulo.grid(row=0, column=0, columnspan=4, padx=1, pady=1, sticky=W + E)
+
+producto = Label(root, text="Fecha")
 producto.grid(row=1, column=0, sticky=W)
-cantidad=Label(root, text="Cantidad")
+cantidad = Label(root, text="Tarjeta de credito")
 cantidad.grid(row=2, column=0, sticky=W)
-precio=Label(root, text="Precio")
+precio = Label(root, text="Descripcion del gasto")
 precio.grid(row=3, column=0, sticky=W)
+precio = Label(root, text="Cuota actual")
+precio.grid(row=4, column=0, sticky=W)
+precio = Label(root, text="Cuota final")
+precio.grid(row=5, column=0, sticky=W)
+precio = Label(root, text="Monto del gasto")
+precio.grid(row=6, column=0, sticky=W)
+
 
 # Defino variables para tomar valores de campos de entrada
-a_val, b_val, c_val = StringVar(), DoubleVar(), DoubleVar()
+a_val, b_val, c_val, d_val, e_val, f_val = (
+    StringVar(),
+    StringVar(),
+    StringVar(),
+    DoubleVar(),
+    DoubleVar(),
+    DoubleVar(),
+)
 w_ancho = 20
 
-entrada1 = Entry(root, textvariable = a_val, width = w_ancho) 
-entrada1.grid(row = 1, column = 1)
-entrada2 = Entry(root, textvariable = b_val, width = w_ancho) 
-entrada2.grid(row = 2, column = 1)
-entrada3 = Entry(root, textvariable = c_val, width = w_ancho) 
-entrada3.grid(row = 3, column = 1)
+entrada1 = Entry(root, textvariable=a_val, width=w_ancho)
+entrada1.grid(row=1, column=1)
+entrada2 = Entry(root, textvariable=b_val, width=w_ancho)
+entrada2.grid(row=2, column=1)
+entrada3 = Entry(root, textvariable=c_val, width=w_ancho)
+entrada3.grid(row=3, column=1)
+entrada4 = Entry(root, textvariable=d_val, width=w_ancho)
+entrada4.grid(row=4, column=1)
+entrada5 = Entry(root, textvariable=e_val, width=w_ancho)
+entrada5.grid(row=5, column=1)
+entrada6 = Entry(root, textvariable=f_val, width=w_ancho)
+entrada6.grid(row=6, column=1)
 
 # --------------------------------------------------
 # TREEVIEW
 # --------------------------------------------------
 
 tree = ttk.Treeview(root)
-tree["columns"]=("col1", "col2", "col3")
+tree["columns"] = ("col1", "col2", "col3", "col4", "col5", "col6")
 tree.column("#0", width=90, minwidth=50, anchor=W)
 tree.column("col1", width=200, minwidth=80)
 tree.column("col2", width=200, minwidth=80)
 tree.column("col3", width=200, minwidth=80)
+tree.column("col4", width=200, minwidth=80)
+tree.column("col5", width=200, minwidth=80)
+tree.column("col6", width=200, minwidth=80)
 tree.heading("#0", text="ID")
-tree.heading("col1", text="Producto")
-tree.heading("col2", text="cantidad")
-tree.heading("col3", text="precio")
+tree.heading("col1", text="Fecha")
+tree.heading("col2", text="Tarjeta de credito")
+tree.heading("col3", text="descripcion")
+tree.heading("col4", text="Cuota actual")
+tree.heading("col5", text="Cuota final")
+tree.heading("col6", text="Monto")
 tree.grid(row=10, column=0, columnspan=4)
 
-boton_alta=Button(root, text="Alta", command=lambda:alta(a_val.get(), b_val.get(), c_val.get(), tree))
-boton_alta.grid(row=6, column=1)
+boton_alta = Button(
+    root,
+    text="Crear gasto",
+    command=lambda: alta(
+        a_val.get(),
+        b_val.get(),
+        c_val.get(),
+        d_val.get(),
+        e_val.get(),
+        f_val.get(),
+        tree,
+    ),
+)
+boton_alta.grid(row=1, column=2)
 
-boton_consulta=Button(root, text="Consultar", command=lambda:consultar())
-boton_consulta.grid(row=7, column=1)
+boton_consulta = Button(root, text="Consultar gastos", command=lambda: consultar(tree))
+boton_consulta.grid(row=2, column=2)
 
-boton_borrar=Button(root, text="Borrar", command=lambda:borrar(tree))
-boton_borrar.grid(row=8, column=1)
+boton_borrar = Button(root, text="Borrar gastos", command=lambda: borrar(tree))
+boton_borrar.grid(row=3, column=2)
 root.mainloop()
-
-
